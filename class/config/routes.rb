2a9f2fb850f7e071f5f9a5075047c170f528c7ae
Abtin Bateni <abtinbateni@gmail.com>
2019-11-02 09:52:34 -0700

    delete everything except essential files

--- class/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :tweets, except: :index
-  root "tweets#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
