50fef13a3ec6a0cc4066ff078c5c7fc7b0ed282a
ishtiyaque <ishtiyaque.2197@yahoo.com>
2019-11-04 21:43:07 -0800

    fix bug

--- class/app/controllers/users_controller.rb
+++ class/app/controllers/users_controller.rb
@@ -64,7 +64,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      begin
+        @user = User.find(params[:id])
+      rescue
+        head 403
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
