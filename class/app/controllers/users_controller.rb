678937f17ad9a64251f9f76022574b63c981c502
ishtiyaque <ishtiyaque.2197@yahoo.com>
2019-11-27 16:19:13 -0800

    add invitations and comments in createbulk

--- class/app/controllers/users_controller.rb
+++ class/app/controllers/users_controller.rb
@@ -73,9 +73,16 @@ class UsersController < ApplicationController
     
     @i = 1
     while @i <= @total_users do
-      @event_id = rand(1 .. @total_events)
-      @invitation = Invitation.new(:user_id => @i, :event_id => @event_id)
-      @invitation.save
+      @invitation_count = rand(10 .. 20)
+      @k = 1
+      while @k <= @invitation_count
+        @event_id = rand(1 .. @total_events)
+        @invitation = Invitation.new(:user_id => @i, :event_id => @event_id)
+        @invitation.save
+        @comment = Comment.new(:user_id => @i, :event_id => @event_id, :body => rand(36**100).to_s(36))
+        @comment.save
+        @k += 1
+      end
       @i += 1
     end
     
