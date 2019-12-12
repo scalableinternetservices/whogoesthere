ff84584e6c282500538ef032cb8ff136bf6a29ff
Abtin Bateni <abtinbateni@gmail.com>
2019-12-12 13:39:31 -0800

    add front-end

---
+++ front-end/precache-manifest.0ca461d856d08f3d7b40a998b11cd3e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3a787b3f31f2e6c8429",
+    "url": "/static/js/main.f3a787b3.chunk.js"
+  },
+  {
+    "revision": "a0a21a82d4121caead67",
+    "url": "/static/js/1.a0a21a82.chunk.js"
+  },
+  {
+    "revision": "f3a787b3f31f2e6c8429",
+    "url": "/static/css/main.4fe9ef0e.chunk.css"
+  },
+  {
+    "revision": "a0a21a82d4121caead67",
+    "url": "/static/css/1.b6098406.chunk.css"
+  },
+  {
+    "revision": "df6833dcf9067a6fb35660cfa71282e1",
+    "url": "/index.html"
+  }
+];
